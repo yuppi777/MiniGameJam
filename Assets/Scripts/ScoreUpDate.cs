f72f67d5602059c5e9235c3736a77c64c2b2e9dc
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2022-10-20 22:32:29 +0900

    OnLoad削除

--- Assets/Scripts/ScoreUpDate.cs
+++ Assets/Scripts/ScoreUpDate.cs
@@ -9,10 +9,6 @@ public class ScoreUpDate : MonoBehaviour
     [SerializeField] Text scoreText;
 
 
-    private void Start()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
     // Update is called once per frame
     void Update()
     {
